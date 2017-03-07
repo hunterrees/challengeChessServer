134478877abd5815406ebf35c9d226cb3be92c5e
hunterrees <hunter.rees13@gmail.com>
2017-03-07 11:45:48 -0700

    Added UserInfo class
    This class removes the password field. This allows for getUser to return the user object without worrying about passwords being leaked.

--- src/main/java/model/UserInfo.java
+++ src/main/java/model/UserInfo.java
@@ -3,10 +3,9 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User {
+public class UserInfo {
 
   private String username;
-  private String password;
   private String email;
   private boolean online;
   private List<String> friends;
@@ -15,9 +14,8 @@ public class User {
   private int loses;
   private int draws;
 
-  public User(String username, String password, String email) {
+  public UserInfo(String username, String email) {
     this.username = username;
-    this.password = password;
     this.email = email;
     online = true;
     friends = new ArrayList<>();
@@ -27,28 +25,23 @@ public class User {
     draws = 0;
   }
 
+  public UserInfo(User user) {
+    this.username = user.getUsername();
+    this.email = user.getEmail();
+    this.online = user.isOnline();
+    this.friends = user.getFriends();
+    this.rank = user.getRank();
+    this.wins = user.getWins();
+    this.loses = user.getLoses();
+    this.draws = user.getDraws();
+  }
+
   public String getUsername() {
     return username;
   }
 
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
   public void setEmail(String email) {
     this.email = email;
-  }
-
-  public boolean isOnline() {
-    return online;
   }
 
   public void setOnline() {
@@ -59,40 +52,20 @@ public class User {
     this.online = false;
   }
 
-  public List<String> getFriends() {
-    return friends;
-  }
-
   public void addFriend(String friend) {
     friends.add(friend);
-  }
-
-  public int getRank() {
-    return rank;
   }
 
   public void setRank(int rank) {
     this.rank = rank;
   }
 
-  public int getWins() {
-    return wins;
-  }
-
   public void addWin() {
     wins++;
   }
 
-  public int getLoses() {
-    return loses;
-  }
-
   public void addLoss() {
     loses++;
-  }
-
-  public int getDraws() {
-    return draws;
   }
 
   public void addDraw() {
