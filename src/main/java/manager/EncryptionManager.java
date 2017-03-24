51b890b85d32c2ce73ab732e53f086277dd57501
hunterrees <hunter.rees13@gmail.com>
2017-03-23 18:17:23 -0600

    Completed EncryptionManager class and unit tests

--- src/main/java/manager/EncryptionManager.java
+++ src/main/java/manager/EncryptionManager.java
@@ -1,20 +1,17 @@
 package manager;
 
+import exception.ServerException;
 import exception.user.UserNotFoundException;
 import model.DHParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.DHParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.HashMap;
@@ -25,38 +22,40 @@ public class EncryptionManager {
   private static final Logger LOGGER = LoggerFactory.getLogger(EncryptionManager.class);
 
   private static final int RANDOM_NUMBER_BIT_LENGTH = 500;
-  private static final String ENCRYPTION_MODE = "AES/CBC/PKCS5Padding";
-  private static final String ENCRYPTION_ALGORITHM = "AES";
+  static final String ENCRYPTION_MODE = "AES/CBC/PKCS5Padding";
+  static final String ENCRYPTION_ALGORITHM = "AES";
 
-  Map<String, byte[]> sharedKeys;
+  private Cipher cipher;
+  AlgorithmParameterSpec parameterSpec;
+  private DHParameterSpec params;
 
   private Map<String, BigInteger> privateExponents;
-  private AlgorithmParameterSpec parameterSpec;
-  private DHParameterSpec params;
-  private Cipher cipher;
+  Map<String, byte[]> sharedKeys;
 
   private static EncryptionManager instance;
 
-  EncryptionManager() throws NoSuchAlgorithmException, NoSuchPaddingException {
+  EncryptionManager() {
     sharedKeys = new HashMap<>();
     privateExponents = new HashMap<>();
 
-    BigInteger g = BigInteger.valueOf(5);
-    BigInteger p = BigInteger.probablePrime(RANDOM_NUMBER_BIT_LENGTH, new SecureRandom());
-    params = new DHParameterSpec(p, g);
-    byte[] IV = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    parameterSpec = new IvParameterSpec(IV);
-    cipher = Cipher.getInstance(ENCRYPTION_MODE);
+    try {
+      BigInteger g = BigInteger.valueOf(5);
+      BigInteger p = BigInteger.probablePrime(RANDOM_NUMBER_BIT_LENGTH, new SecureRandom());
+      params = new DHParameterSpec(p, g);
+      byte[] IV = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+      parameterSpec = new IvParameterSpec(IV);
+      cipher = Cipher.getInstance(ENCRYPTION_MODE);
+    }
+    catch (GeneralSecurityException e){
+      LOGGER.error("Something went wrong when creating the encryption manager");
+      throw new ServerException(e);
+    }
   }
 
   public static EncryptionManager getInstance() {
-    if (instance == null)
-      try {
-        instance = new EncryptionManager();
-      }
-      catch (GeneralSecurityException e){
-        LOGGER.error("Something went wrong when creating the encryption manager");
-      }
+    if (instance == null) {
+      instance = new EncryptionManager();
+    }
     return instance;
   }
 
@@ -67,13 +66,13 @@ public class EncryptionManager {
    * @param username non-null string of the user who is requesting the Diffie-Hellman key.
    * @return public Diffie-Hellman parameters.
    */
-  public DHParams getParameters(String username) {
+  public DHParams getPublicParameters(String username) {
     if (privateExponents.get(username) == null) {
       BigInteger s = BigInteger.probablePrime(RANDOM_NUMBER_BIT_LENGTH, new SecureRandom());
       privateExponents.put(username, s);
     }
     BigInteger publicParam = modExp(params.getG(), privateExponents.get(username), params.getP());
-    LOGGER.info("Generating public parameter {] to {]", publicParam, username);
+    LOGGER.info("Generating public parameter {} to {}", publicParam, username);
     return new DHParams(params, publicParam.toByteArray());
   }
 
@@ -84,7 +83,7 @@ public class EncryptionManager {
    * @param parameters Diffie-Hellman parameters of the user.
    * @throws UserNotFoundException if user hasn't requested the initial public parameters.
    */
-  public void setUpSharedKey(@PathVariable String username, @RequestBody DHParams parameters) throws UserNotFoundException {
+  public void generateSharedKey(String username, DHParams parameters) throws UserNotFoundException {
     LOGGER.info("Setting up shared key for {} with parameters {}", username, parameters);
     BigInteger publicParam = new BigInteger(parameters.getPublicParam());
     if (privateExponents.get(username) == null) {
@@ -103,13 +102,22 @@ public class EncryptionManager {
    *
    * @param username non-null string of the user who's password is being decrypted.
    * @param password non-null string containing the encrypted password.
+   * @throws UserNotFoundException when the user does not have a shared key set up
    * @return decrypted password
-   * @throws GeneralSecurityException if algorithm does not exist.
    */
-  public String decryptPassword(String username, String password) throws GeneralSecurityException {
-    SecretKeySpec key = new SecretKeySpec(sharedKeys.get(username), ENCRYPTION_ALGORITHM);
-    cipher.init(Cipher.DECRYPT_MODE, key, parameterSpec);
-    return new String(cipher.doFinal(password.getBytes()));
+  public String decryptPassword(String username, byte[] password) throws UserNotFoundException {
+    try {
+      LOGGER.info("Decrypting password for {}", username);
+      if (!sharedKeys.containsKey(username)) {
+        throw new UserNotFoundException(String.format("User %s does not have a shared key", username));
+      }
+      SecretKeySpec key = new SecretKeySpec(sharedKeys.get(username), ENCRYPTION_ALGORITHM);
+      cipher.init(Cipher.DECRYPT_MODE, key, parameterSpec);
+      return new String(cipher.doFinal(password), "UTF-8");
+    } catch (Exception e) {
+      LOGGER.error("Something went wrong during decryption of a password");
+      throw new ServerException(e);
+    }
   }
 
   BigInteger modExp(BigInteger x, BigInteger y, BigInteger n) {
